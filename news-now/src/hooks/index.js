8cf74c12baa67c64b2e635148887baa1b09c6007
HerbertHomolka1 <herberthomolka0@gmail.com>
2023-11-10 00:57:42 +0100

    working useNews hook. Demonstrated by displaying article titles on the screen

---
+++ news-now/src/hooks/index.js
@@ -0,0 +1,6 @@
+// export { default as hookName } from "./source";
+// we will write one such export for each hook in the src/hooks folder.
+// this way we will be able to import all of the hooks as an object as shown below:
+// import { useHooks1, useHook2, useHook3 } from "./hooks";
+
+export { default as useNews } from "./useNews";

470d14642c7c72c5f69d3f8e4f4ab6ae8b8d5bb1
Alex <amaletskyi1@gmail.com>
2023-11-12 12:45:55 +0100

    Article card component

--- news-now/src/pages/AllNewsPage.jsx
+++ news-now/src/pages/AllNewsPage.jsx
@@ -5,7 +5,19 @@ const AllNewsPage = () => {
   return (
     <div>
       <Navbar />
-      AllNewsPage
+      <div className="article-card">
+          <div className="book-details-parts">
+              <img src={urlToImage} alt={title} />
+          </div>
+          <div className="details">
+              <p className="date">{publishedAt}</p>
+              {/* <p classname="category">Business</p> */}
+          </div>
+          <div className="content">
+              <h2 className="title">{title}</h2>
+              <p className="description">{description}</p>
+          </div>
+      </div>
     </div>
   )
 }

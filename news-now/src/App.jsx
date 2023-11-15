910144af247d638bf28ba1f29bde0cdae1294966
HYF BE <damaksinaarseniya@gmail.com>
2023-11-15 23:15:08 +0100

    Added the global context

--- news-now/src/App.jsx
+++ news-now/src/App.jsx
@@ -1,22 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { GlobalStateProvider } from "./context/GlobalStateContext";
 
-import './App.css';
-import AllNewsPage from './pages/AllNewsPage';
-import ArticlePage from './pages/ArticlePage';
-import PaymentsPage from './pages/PaymentsPage';
-import LogoutPage from './pages/LogoutPage';
+import "./App.css";
+import AllNewsPage from "./pages/AllNewsPage";
+import ArticlePage from "./pages/ArticlePage";
+import PaymentsPage from "./pages/PaymentsPage";
+import LogoutPage from "./pages/LogoutPage";
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<AllNewsPage />} />
-                <Route path="/article/:id" element={<ArticlePage />} />
-                <Route path="/payments" element={<PaymentsPage />} />
-                <Route path="/logout" element={<LogoutPage />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <GlobalStateProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<AllNewsPage />} />
+          <Route path="/article/:id" element={<ArticlePage />} />
+          <Route path="/payments" element={<PaymentsPage />} />
+          <Route path="/logout" element={<LogoutPage />} />
+        </Routes>
+      </Router>
+    </GlobalStateProvider>
+  );
 };
 
 export default App;

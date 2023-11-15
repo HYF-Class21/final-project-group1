d3aea9f9b885837058b4d51cffd99f4bc6a7b2e6
HYF BE <damaksinaarseniya@gmail.com>
2023-11-15 22:48:59 +0100

    Added login form

---
+++ news-now/src/components/LoginForm.jsx
@@ -0,0 +1,74 @@
+import styles from "./LoginForm.module.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Cookies from 'js-cookie';
+
+import validateEmail from "../utils/validateEmail.js";
+import validatePassword from "../utils/validatePassword.js";
+
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const emailIsValid = validateEmail(email);
+    const passwordIsValid = validatePassword(password);
+    if (emailIsValid && passwordIsValid) {
+      const login = async () => {
+        try {
+          Cookies.set('email', email);
+          setEmail("");
+          setPassword("");
+          navigate("/article/1");
+        } catch {
+          setError("Error while log in");
+        }
+      };
+      login();
+    } else {
+      setError("Email or password is not valid");
+    }
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div>
+      <p className={error}>{error}</p>
+      <div className={styles.formContainer}>
+        <h1 className={styles.formContainerH1}>Login form</h1>
+        <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
+          <label htmlFor="email">Email: </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => handleEmail(e)}
+          />
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => handlePassword(e)}
+          />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
--- news-now/src/pages/AllNewsPage.jsx
+++ news-now/src/pages/AllNewsPage.jsx
@@ -1,11 +1,13 @@
 import styles from './AllNewsPage.module.css';
 import Navbar from '../components/Navbar';
+import LoginForm from '../components/LoginForm';
 
 const AllNewsPage = () => {
   return (
     <div>
       <Navbar />
       AllNewsPage
+      <LoginForm />
     </div>
   )
 }

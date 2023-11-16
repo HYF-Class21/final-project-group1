bae93bef0fa78ad313c8bc7a492f9b5aa8f5339b
HerbertHomolka1 <herberthomolka0@gmail.com>
2023-11-16 03:02:49 +0100

    added card, filters now work

--- news-now/src/pages/AllNewsPage.jsx
+++ news-now/src/pages/AllNewsPage.jsx
@@ -1,24 +1,25 @@
 import styles from "./AllNewsPage.module.css";
-import { useGlobalState } from '../context/GlobalStateContext';
-
+import { useGlobalState } from "../context/GlobalStateContext";
+import { useState } from "react";
 import Navbar from "../components/Navbar";
 
 import { useNews } from "../hooks";
 
-const AllNewsPage = () => {
-  const {
-    isLoggedIn,
-    setIsLoggedIn,
-    isPayed,
-    setIsPayed
-  } = useGlobalState();
+import Card from "../components/Card";
 
+const AllNewsPage = () => {
+  const { isLoggedIn, setIsLoggedIn, isPayed, setIsPayed } = useGlobalState();
+  const [filters, setFilters] = useState({ country: "us", category: "sports" });
 
   let data = [];
 
-  for (let news of useNews("us", "sports", "bbc-news")) {
-    data.push(news);
+  for (let article of useNews(filters)) {
+    data.push(article);
   }
+
+  const handleCountryChange = (event) => {
+    setFilters((filters) => ({ ...filters, country: event.target.value }));
+  };
 
   return (
     <>
@@ -26,14 +27,52 @@ const AllNewsPage = () => {
         <Navbar />
         AllNewsPage
       </div>
-      {data.map((news) => (
-        <div key={news.title}>
-          <a href={news.url}>{news.title}</a>
-          <div>{news.author}</div>
-          <div>{news.description}</div>
-          <div>{news.content}</div> <hr />
+      <div>
+        <div>
+          <label>
+            <input
+              type="radio"
+              value="us"
+              checked={filters.country === "us"}
+              onChange={handleCountryChange}
+            />
+            United States
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              value="be"
+              checked={filters.country === "be"}
+              onChange={handleCountryChange}
+            />
+            Belgium
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              value="pt"
+              checked={filters.country === "pt"}
+              onChange={handleCountryChange}
+            />
+            Portugal
+          </label>
+
+          <p>Selected Option: {filters.country}</p>
         </div>
-      ))}
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          flexWrap: "wrap",
+        }}
+      >
+        {data.map((article) => (
+          <Card key={article.title} article={article} />
+        ))}
+      </div>
     </>
   );
 };

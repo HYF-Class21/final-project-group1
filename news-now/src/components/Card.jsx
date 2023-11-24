3b27acbbdaef36af3b0320715158b6bf0af93f37
HYF BE <damaksinaarseniya@gmail.com>
2023-11-24 19:30:39 +0100

    layout grid

--- news-now/src/components/Card.jsx
+++ news-now/src/components/Card.jsx
@@ -1,25 +1,71 @@
 import styles from "./Card.module.css";
 import { Link } from "react-router-dom";
 // import { useGlobalState } from "../context/GlobalStateContext";
+import formatDate from "../utils/formatDate";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendarDays,
+  faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Card = ({ article, index }) => {
+  const categoryColors = [
+    ["us", "yellow"],
+    ["ie", "#e74c3c"],
+    ["pl", "#2ecc71"],
+    ["health", "#1abc9c"],
+    ["entertainment", "#d35400"],
+    ["general", "#27ae60"],
+    ["science", "#f1c40f"],
+    ["sports", "#8e44ad"],
+    ["technology", "#3498db"],
+  ];
+
+  const getCategoryColor = (category) => {
+    const foundCategory = categoryColors.find(
+      ([cat, color]) => cat === category
+    );
+    return foundCategory ? foundCategory[1] : "#ccc";
+  };
+  const categoryColor = getCategoryColor(article.category);
+  const countryColor = getCategoryColor(article.country);
+
   return (
-    <div className={`${styles.card} ${styles['art' + index]}`}>
-      <div className={styles.img}>
+    <div className={`${styles.card} ${styles["art" + index]}`}>
+      <div className={styles.imgDiv}>
         <img
-          style={{ width: "100%", height: "100%" }}
+          style={{ maxWidth: "100%", maxHeight: "100%" }}
           src={article.image}
           alt={article.title}
         />
       </div>
-      <div className={styles.border}></div>
-      <div className={styles.border}>
-        <div className={styles.bold}>{article.title}</div>
-        <br />
-        <div className={styles.gray}>{article.author}</div>
+      <div className={styles.dateDiv}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <FontAwesomeIcon icon={faCalendarDays} style={{ color: "gray" }} />
+          <div className={styles.date}>{formatDate(article.publishedAt)}</div>
+        </div>
+        <div className={styles.categoryDiv}>
+          <div
+            className={styles.category}
+            style={{ backgroundColor: categoryColor }}
+          >
+            {article.category}
+          </div>
+          <div
+            className={styles.category}
+            style={{ backgroundColor: countryColor }}
+          >
+            {article.country}
+          </div>
+        </div>
       </div>
-
-      <div className={styles.border}>{article.description}</div>
+      <div className={styles.title}>{article.title}</div>
+      <div className={styles.description}>{article.description}..</div>
+      <div className={styles.buttonDiv}>
+        <button className={styles.button}>
+          Read More <FontAwesomeIcon icon={faArrowRight} />
+        </button>
+      </div>
     </div>
   );
 };
--- news-now/src/pages/AllNewsPage.jsx
+++ news-now/src/pages/AllNewsPage.jsx
@@ -9,7 +9,6 @@ import { articles } from "../data";
 import Card from "../components/Card";
 import RadioButton from "../components/RadioButton";
 
-
 const AllNewsPage = () => {
   const { isLoggedIn, setIsLoggedIn, isPayed, setIsPayed } = useGlobalState();
   const [filters, setFilters] = useState({ country: "us", category: "sports" });
@@ -26,6 +25,8 @@ const AllNewsPage = () => {
     }
   }
 
+  console.log(data);
+
   const handleCountryChange = (event) => {
     setFilters((filters) => ({ ...filters, country: event.target.value }));
   };
@@ -34,42 +35,88 @@ const AllNewsPage = () => {
   };
 
   return (
-    <>
-      <div>
-        <Navbar />
-        AllNewsPage
-      </div>
-      <div>
+    <div className={styles.newsContainer}>
+      <Navbar />
+      <div className={styles.contentContainer}>
         <div>
-          <RadioButton value='us' name='United States' handleRadioChange={handleCountryChange} filters={filters} btype ='country' />
-          <RadioButton value='ie' name='Ireland' handleRadioChange={handleCountryChange} filters={filters} btype ='country'/>
-          <RadioButton value='pl' name='Poland' handleRadioChange={handleCountryChange} filters={filters} btype ='country'/>
+          <RadioButton
+            value="us"
+            name="United States"
+            handleRadioChange={handleCountryChange}
+            filters={filters}
+            btype="country"
+          />
+          <RadioButton
+            value="ie"
+            name="Ireland"
+            handleRadioChange={handleCountryChange}
+            filters={filters}
+            btype="country"
+          />
+          <RadioButton
+            value="pl"
+            name="Poland"
+            handleRadioChange={handleCountryChange}
+            filters={filters}
+            btype="country"
+          />
 
           <p>Selected Option: {filters.country}</p>
         </div>
-      </div>
-
-      <div>
         <div>
-          <RadioButton value="health" handleRadioChange={handleCategoryChange} filters={filters} btype ='category'/>
-          <RadioButton value="entertainment" handleRadioChange={handleCategoryChange} filters={filters} btype ='category'/>
-          <RadioButton value="general" handleRadioChange={handleCategoryChange} filters={filters} btype ='category'/>
-          <RadioButton value="science" handleRadioChange={handleCategoryChange} filters={filters} btype ='category'/>
-          <RadioButton value="sports" handleRadioChange={handleCategoryChange} filters={filters} btype ='category'/>
-          <RadioButton value="buisness" handleRadioChange={handleCategoryChange} filters={filters} btype ='category'/>
-          <RadioButton value="technology" handleRadioChange={handleCategoryChange} filters={filters} btype ='category'/>
-      
+          <RadioButton
+            value="health"
+            handleRadioChange={handleCategoryChange}
+            filters={filters}
+            btype="category"
+          />
+          <RadioButton
+            value="entertainment"
+            handleRadioChange={handleCategoryChange}
+            filters={filters}
+            btype="category"
+          />
+          <RadioButton
+            value="general"
+            handleRadioChange={handleCategoryChange}
+            filters={filters}
+            btype="category"
+          />
+          <RadioButton
+            value="science"
+            handleRadioChange={handleCategoryChange}
+            filters={filters}
+            btype="category"
+          />
+          <RadioButton
+            value="sports"
+            handleRadioChange={handleCategoryChange}
+            filters={filters}
+            btype="category"
+          />
+          {/* <RadioButton
+            value="buisness"
+            handleRadioChange={handleCategoryChange}
+            filters={filters}
+            btype="category"
+          /> */}
+          <RadioButton
+            value="technology"
+            handleRadioChange={handleCategoryChange}
+            filters={filters}
+            btype="category"
+          />
 
           <p>Selected Option: {filters.category}</p>
         </div>
-      </div>
 
-      <div className={styles.articlesContainer}>
-        {data.slice(0, 8).map((article, index) => {
-          return <Card key={article.title} article={article} index={index}/>;
-        })}
+        <div className={styles.articlesContainer}>
+          {data.slice(0, 10).map((article, index) => {
+            return <Card key={article.title} article={article} index={index} />;
+          })}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 

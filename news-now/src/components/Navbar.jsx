7439500d7062ea9c83d0d2fe0d6bce57f5655bae
HYF BE <damaksinaarseniya@gmail.com>
2023-12-01 00:33:37 +0100

    underline a

--- news-now/src/components/Navbar.jsx
+++ news-now/src/components/Navbar.jsx
@@ -57,8 +57,8 @@ const Navbar = () => {
       </div>
       <div className={styles.logo}></div>
       <ul className={styles.ul}>
-        <li className={`${styles.li} ${location.pathname === "/" ? styles.activeLink : ""}`}>
-          <Link className={styles.a} to="/">
+        <li className={styles.li}>
+          <Link className={`${styles.a} ${location.pathname === "/" ? styles.activeLink : ""}`} to="/">
             Home
           </Link>
           <FontAwesomeIcon
